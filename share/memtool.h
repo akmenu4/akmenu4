1e64d7bcce5f3e0840e3bdb0f7d7f3ad2473492f
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2026-03-30 02:56:53 -0700

    memtool: explicitly specify needed headers to avoid compilation error

--- share/memtool.h
+++ share/memtool.h
@@ -6,6 +6,9 @@
 
 #pragma once
 
+#include <nds/bios.h>
+#include <nds/ndstypes.h>
+
 inline void fillMemory(void* addr, u32 count, u32 value) {
     swiFastCopy((void*)(&value), addr, (count >> 2) | COPY_MODE_WORD | COPY_MODE_FILL);
 }
