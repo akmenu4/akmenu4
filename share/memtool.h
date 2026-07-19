33f2ae9278fc12111b56cb8e849b3b180ef06f49
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2026-07-19 14:14:47 -0700

    memtool: make functions static inline
    In file included from arm7/source/main.c:10:
    share/memtool.h:13:5: warning: 'swiFastCopy' is static but used in inline function 'fillMemory' which is not static
       13 |     swiFastCopy((void*)(&value), addr, (count >> 2) | COPY_MODE_WORD | COPY_MODE_FILL);
          |     ^~~~~~~~~~~

--- share/memtool.h
+++ share/memtool.h
@@ -9,10 +9,10 @@
 #include <nds/bios.h>
 #include <nds/ndstypes.h>
 
-inline void fillMemory(void* addr, u32 count, u32 value) {
+static inline void fillMemory(void* addr, u32 count, u32 value) {
     swiFastCopy((void*)(&value), addr, (count >> 2) | COPY_MODE_WORD | COPY_MODE_FILL);
 }
 
-inline void zeroMemory(void* addr, u32 count) {
+static inline void zeroMemory(void* addr, u32 count) {
     fillMemory(addr, count, 0);
 }
